5f0e287f92c4ce82d6e0186ec4bb6a7aeb1f3de5
moss6445 <yys.chris@gmail.com>
2026-03-22 12:34:53 +0800

    fix: migrate middleware.ts to proxy.ts for Next.js 16
    Cherry-picked from local main (7bd3b61)

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
 export default withAuth(
-  function middleware(req) {
+  function proxy(req) {
     const { nextUrl } = req
     const isLoggedIn = !!req.nextauth.token
 
